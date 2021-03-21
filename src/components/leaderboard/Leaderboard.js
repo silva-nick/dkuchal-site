9a2bdc269ca3a2192ebea167e2cd02087adc9e06
Nick Silva <silva.d.nicholas@gmail.com>
2021-03-20 23:48:19 -0500

    fix leaderboard status

--- src/components/leaderboard/Leaderboard.js
+++ src/components/leaderboard/Leaderboard.js
@@ -14,7 +14,7 @@ import Footer from "../navigation/Footer";
 import NavBar from "../navigation/NavBar";
 
 import { getLeaders } from "../../api/api";
-import { UpArrow, DownArrow, Flag } from "./Vectors";
+import { UpArrow, DownArrow, Flag, Circle } from "./Vectors";
 
 class Leaderboard extends React.Component {
   constructor() {
@@ -48,49 +48,57 @@ class Leaderboard extends React.Component {
       height: "100%",
     };
 
-    let cards = this.state.leaders.map((leader, index) => (
-      <ListGroup.Item key={index}>
-        <Row>
-          <Col lg={0} xs={0} style={{ paddingRight: 0 }}>
-            <div style={{ ...displayCenter, padding: 0 }}>
-              {"#" + (index + 1)}
-            </div>
-          </Col>
-          <Col lg={0} xs={0} style={{ ...displayCenter, padding: 0 }}>
-            <div>
-              <Image
-                src={leader.picture[0].thumbnails.large.url}
-                roundedCircle
-                style={{
-                  width: "3rem",
-                  height: "3rem",
-                  float: "left",
-                  margin: 0,
-                  objectFit: "cover",
-                }}
-              />
-            </div>
-          </Col>
-          <Col lg={10} xs={8}>
-            <div style={{ ...displayCenter, lineHeight: ".4rem" }}>
-              <p>{leader.nameone + " and " + leader.nametwo}</p>
-              <p style={{ color: "#a4a4a4", margin: 0, fontSize: "small" }}>
-                {leader.points + " Points"}
-              </p>
-            </div>
-          </Col>
-          {this.state.innerWidth > 500 && (
-            <Col lg={0} xs={0}>
-              <div style={{ ...displayCenter }}>
-                <div>
-                  <UpArrow />
-                </div>
+    let cards = this.state.leaders.map((leader, index) => {
+      let symbol;
+      if (leader.status > 0) {
+        symbol = <UpArrow />;
+      } else if (leader.status === 0) {
+        symbol = <Circle />;
+      } else {
+        symbol = <DownArrow />;
+      }
+      return (
+        <ListGroup.Item key={index}>
+          <Row>
+            <Col lg={0} xs={0} style={{ paddingRight: 0 }}>
+              <div style={{ ...displayCenter, padding: 0 }}>
+                {"#" + (index + 1)}
               </div>
             </Col>
-          )}
-        </Row>
-      </ListGroup.Item>
-    ));
+            <Col lg={0} xs={0} style={{ ...displayCenter, padding: 0 }}>
+              <div>
+                <Image
+                  src={leader.picture[0].thumbnails.large.url}
+                  roundedCircle
+                  style={{
+                    width: "3rem",
+                    height: "3rem",
+                    float: "left",
+                    margin: 0,
+                    objectFit: "cover",
+                  }}
+                />
+              </div>
+            </Col>
+            <Col lg={10} xs={8}>
+              <div style={{ ...displayCenter, lineHeight: ".4rem" }}>
+                <p>{leader.nameone + " and " + leader.nametwo}</p>
+                <p style={{ color: "#a4a4a4", margin: 0, fontSize: "small" }}>
+                  {leader.points + " Points"}
+                </p>
+              </div>
+            </Col>
+            {this.state.innerWidth > 500 && (
+              <Col lg={0} xs={0}>
+                <div style={{ ...displayCenter }}>
+                  <div>{symbol}</div>
+                </div>
+              </Col>
+            )}
+          </Row>
+        </ListGroup.Item>
+      );
+    });
 
     return this.state.loading ? (
       <center style={{ width: "100%", height: "100%", padding: "60% 0" }}>
