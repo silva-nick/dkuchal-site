99dbbfb2d82249e9c0e073a1bf3e0a30d419c906
Nick Silva <silva.d.nicholas@gmail.com>
2021-03-22 09:33:32 -0500

    hide tasks and leaderboad

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -15,19 +15,19 @@ function NavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse>
           <Nav>
-            <Nav.Link as={Link} to="/tasks">
+            {/*<Nav.Link as={Link} to="/tasks">
               Tasks
-            </Nav.Link>
+            </Nav.Link>*/}
             <Nav.Link as={Link} to="/shop">
               Shop
             </Nav.Link>
             <Nav.Link as={Link} to="/about">
               About
             </Nav.Link>
-            <Nav.Link as={Link} to="/leaderboard">
+            {/*<Nav.Link as={Link} to="/leaderboard">
               Leaderboard
-            </Nav.Link>
-            <Nav.Link as={Link} to="/login" style={{color:"#5289ff"}}>
+            </Nav.Link>*/}
+            <Nav.Link as={Link} to="/login" style={{ color: "#5289ff" }}>
               Login
             </Nav.Link>
           </Nav>
