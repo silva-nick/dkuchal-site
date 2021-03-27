e1b61fd0145e4851a463349a6519981c8dd09ef7
Nick Silva <silva.d.nicholas@gmail.com>
2021-03-27 11:52:56 -0600

    fix submission display, etc.

--- src/components/taskpage/SubmitPage.js
+++ src/components/taskpage/SubmitPage.js
@@ -86,7 +86,7 @@ class SubmitPage extends React.Component {
 
           this.setState({
             ...oldState,
-            showAlert: "Your submission has succeeded. Congrats!",
+            showAlert: "Your new link: " + success,
             success: true,
             filebackupURL: success,
             loading: false,
@@ -432,112 +432,6 @@ class SubmitPage extends React.Component {
             </Card>
           </Container>
         )}
-        <Container>
-          <center style={{ margin: "2rem 0 0 0" }}>
-            <h1>Submit your proof</h1>
-            <br />
-          </center>
-
-          {this.state.showAlert && (
-            <Alert
-              variant={this.state.success ? "success" : "danger"}
-              onClose={() => this.handleAlertClose(false)}
-              dismissible
-              style={{
-                textAlign: "center",
-                margin: "0 0 1rem 0",
-              }}
-            >
-              <Alert.Heading>{this.state.showAlert}</Alert.Heading>
-              <hr />
-              <p style={{ margin: 0 }}>
-                {this.state.success
-                  ? "Check out the shop!"
-                  : "Please try again or contact DKU Challenge admin."}
-              </p>
-            </Alert>
-          )}
-
-          <Card className="text-left" style={{ margin: "0 0 2% 0" }}>
-            <Card.Header>Proof of participation</Card.Header>
-            <Card.Body>
-              <Card.Text className="text-muted">Please be honest.</Card.Text>
-              <Form>
-                <Form.Group controlId="formBasicNames">
-                  <Form.Label>Names</Form.Label>
-                  <Row>
-                    <Col>
-                      <Form.Control
-                        placeholder="First partner name"
-                        value={this.state.nameone}
-                        onChange={(e) =>
-                          this.setState({ nameone: e.target.value })
-                        }
-                        required
-                      />
-                    </Col>
-                    <Col>
-                      <Form.Control
-                        placeholder="Second partner name"
-                        value={this.state.nametwo}
-                        onChange={(e) =>
-                          this.setState({ nametwo: e.target.value })
-                        }
-                        required
-                      />
-                    </Col>
-                  </Row>
-                </Form.Group>
-
-                <Form.Group controlId="formBasicPassword">
-                  <Form.Label>Description</Form.Label>
-                  <Form.Control
-                    as="textarea"
-                    rows={4}
-                    placeholder="Description"
-                    value={this.state.description}
-                    onChange={(e) =>
-                      this.setState({ description: e.target.value })
-                    }
-                    required
-                  />
-                </Form.Group>
-
-                <br />
-                <center>
-                  {" "}
-                  <ButtonGroup toggle>
-                    {this.state.typeSelect.map((type, idx) => (
-                      <ToggleButton
-                        key={idx}
-                        type="radio"
-                        variant="light"
-                        name="radio"
-                        value={type.key}
-                        checked={type.key === this.state.type}
-                        onChange={(e) => this.handleTypeChange(e)}
-                      >
-                        {type.text}
-                      </ToggleButton>
-                    ))}
-                  </ButtonGroup>
-                </center>
-
-                {input}
-
-                <br />
-                <Button variant="primary" type="submit" onClick={this.submit}>
-                  Submit
-                </Button>
-              </Form>
-            </Card.Body>
-            <Card.Footer className="text-muted">
-              Ready to get some prizes?{" "}
-              <Link to="/shop">Check them out here.</Link>
-            </Card.Footer>
-          </Card>
-        </Container>
-
         <FooterLight />
       </div>
     );
